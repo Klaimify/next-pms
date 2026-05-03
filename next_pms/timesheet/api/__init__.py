1c123e82d3f4565cf8c47c6d3382d7e3f75db482
Samkit Jain <samkit5495@gmail.com>
2026-05-03 07:02:03 +0530

    fix: count issue

--- next_pms/timesheet/api/__init__.py
+++ next_pms/timesheet/api/__init__.py
@@ -140,15 +140,11 @@ def get_count(
 ) -> int:
     from frappe.desk.reportview import execute
 
-    distinct = "distinct " if distinct else ""
-    fieldname = f"{distinct}`tab{doctype}`.name"
-
-    fieldname = [f"count({fieldname}) as total_count"]
     count = execute(
         doctype,
         distinct=distinct,
         limit=limit,
-        fields=fieldname,
+        fields=[{"COUNT": "*", "as": "total_count"}],
         filters=filters,
         or_filters=or_filters,
         ignore_permissions=ignore_permissions,
